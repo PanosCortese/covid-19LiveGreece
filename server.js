a92b6cfe2e624b10ffe77164b1de1c8272b7daee
xcovers <ntheodoropoulos@outlook.com>
2020-03-17 11:49:26 +0200

    daf

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ const MongoClient = require('mongodb').MongoClient;
 container.resolve(function(index, scrap_fun){ // brings the modules from the container. we run everyting in here
   // adding mongoose connection to the database
   mongoose.Promise = global.Promise;
-  //const uri = "mongodb+srv://ntheodoropoulos:nikblod1@cluster0-cfrox.mongodb.net/test?retryWrites=true&w=majority"
   mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology: true })
 
   const db = mongoose.connection;
